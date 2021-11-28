ab81172690cfb5685b1b512f1cf467118355ed94
Vlad <grigoriev.vl.vl@yandex.ru>
2021-11-28 19:31:20 +0300

    static files and admin site

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
 
-from .models import Question
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None, {'fields': ['question_text']}),
+            ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
+            ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
