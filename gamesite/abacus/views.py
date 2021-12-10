6b2f4faeb966a1958065715a2802734ffe6111fb
Vlad <grigoriev.vl.vl@yandex.ru>
2021-12-10 21:15:15 +0300

    testuser created

--- gamesite/abacus/views.py
+++ gamesite/abacus/views.py
@@ -9,16 +9,20 @@ from .models import Problem, Contest, Answer
 def index(request):
     return HttpResponse("Hello, world. You're at the abacus index.")
 
+
 def students_screen(request):
     return HttpResponse("Hello, there. You're looking at the student's screen")
 
+
 def problems_list(request):
     return HttpResponse("Hello! You're looking at the list of problems")
+
 
 def problem_details(request, problem_id):
     template = loader.get_template('abacus/problem_text.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
 
 def context_problems(request, contest_id):
     template = loader.get_template('abacus/contest_screen.html')
